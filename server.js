ff586e8ae5770bf32b4bdcb6dc9c4785e814e79b
cme-osuka <98716814+cme-osuka@users.noreply.github.com>
2022-08-15 09:28:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Goodbye World!");
+  res.send("Good morning World!");
 });
 
 app.listen(PORT, () => {
